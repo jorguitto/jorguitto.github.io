13a2169b86efb182d88d9d86b48ae16702b27627
jorguitto <90102249+jorguitto@users.noreply.github.com>
2026-05-08 18:31:38 +0200

    Add files via upload

---
+++ js/core/BioEngine.js
@@ -0,0 +1,13 @@
+﻿import { BIO_DATABASE } from '../data/bioDatabase.js';
+import { CLIMA_PETRER } from '../data/clima.js';
+
+export class BioEngine {
+  
+            constructor() {
+                this.weights = JSON.parse(localStorage.getItem('bio_weights')) || {
+                    fatigueSensitivity: 1.2,
+                    sleepEfficiency: 1.0,
+                    recoveryFatigueFactor: 1.0 
+                }
+
+}
