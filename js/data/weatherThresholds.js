996d5b42cc6ad1f25a107d3ad31915fe5054b344
jorge <90102249+jorguitto@users.noreply.github.com>
2026-05-12 23:05:47 +0200

    Delete js directory

--- js/data/weatherThresholds.js
+++
@@ -1,6 +0,0 @@
-/** Umbrales orientativos (no clínicos) para mensajes de confort / esfuerzo térmico. */
-
-export const HEAT_STRESS_TEMP_C = 30;
-export const COLD_STRESS_TEMP_C = 5;
-export const HIGH_UV = 7;
-export const RAIN_PRECIP_MM_H = 0.3;
