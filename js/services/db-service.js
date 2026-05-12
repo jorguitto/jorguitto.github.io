8069e2dc36400defcc847d7d12fea510e427c075
jorge <90102249+jorguitto@users.noreply.github.com>
2026-05-12 19:27:23 +0200

    Delete js directory

--- js/services/db-service.js
+++
@@ -1,9 +0,0 @@
-/**
- * Punto de extensión Firestore / persistencia compartida (reexporta snapshots diarios).
- */
-export {
-  saveDailySnapshotFirestore,
-  loadDailySnapshotFirestore,
-  writeLocalDailySnapshot,
-  readLocalDailySnapshotRecord,
-} from './daily-snapshot-persistence.js';
